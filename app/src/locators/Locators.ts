8ff20e388b471f664570ee8b65871ef4bc59c40a
Romain Fromi <romain.fromi@gmail.com>
2025-05-06 14:37:19 +0200

    Remove PlayerColor

--- app/src/locators/Locators.ts
+++ app/src/locators/Locators.ts
@@ -1,6 +1,5 @@
 import { LocationType } from '@gamepark/double-seven/material/LocationType'
 import { MaterialType } from '@gamepark/double-seven/material/MaterialType'
-import { PlayerColor } from '@gamepark/double-seven/PlayerColor'
 import { Locator } from '@gamepark/react-game'
 
-export const Locators: Partial<Record<LocationType, Locator<PlayerColor, MaterialType, LocationType>>> = {}
+export const Locators: Partial<Record<LocationType, Locator<number, MaterialType, LocationType>>> = {}
--- rules/src/DoubleSevenSetup.ts
+++ rules/src/DoubleSevenSetup.ts
@@ -3,13 +3,12 @@ import { DoubleSevenOptions } from './DoubleSevenOptions'
 import { DoubleSevenRules } from './DoubleSevenRules'
 import { LocationType } from './material/LocationType'
 import { MaterialType } from './material/MaterialType'
-import { PlayerColor } from './PlayerColor'
 import { RuleId } from './rules/RuleId'
 
 /**
  * This class creates a new Game based on the game options
  */
-export class DoubleSevenSetup extends MaterialGameSetup<PlayerColor, MaterialType, LocationType, DoubleSevenOptions> {
+export class DoubleSevenSetup extends MaterialGameSetup<number, MaterialType, LocationType, DoubleSevenOptions> {
   Rules = DoubleSevenRules
 
   setupMaterial(_options: DoubleSevenOptions) {
