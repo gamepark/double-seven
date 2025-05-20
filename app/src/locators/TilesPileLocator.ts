487527bb4a09823aa1c622cb0b70c4363ecf953f
sylvestredavid <david.sylvestre.24@gmail.com>
2025-05-20 12:32:52 +0200

    tuile retournée tout en haut de la pile

--- app/src/locators/TilesPileLocator.ts
+++ app/src/locators/TilesPileLocator.ts
@@ -1,10 +1,20 @@
 /** @jsxImportSource @emotion/react */
-import { PileLocator } from '@gamepark/react-game'
+import { ItemContext, PileLocator } from '@gamepark/react-game'
+import { MaterialItem } from '@gamepark/rules-api'
 
 class TilesPileLocator extends PileLocator {
   radius = 20
   coordinates = { x: 3, y: 0 }
   limit = 91
+
+  getItemCoordinates(item: MaterialItem, context: ItemContext) {
+    const coordinates = super.getItemCoordinates(item, context)
+    if (!item.location.rotation) {
+      coordinates.z = 10
+    }
+    console.log(coordinates)
+    return coordinates
+  }
 }
 
 export const tilesPileLocator = new TilesPileLocator()
