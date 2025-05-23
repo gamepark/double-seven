392c0c7aeb82e1153c8ec9596b1163ce5267520d
Romain Fromi <romain.fromi@gmail.com>
2025-05-23 19:11:51 +0200

    Upgrade framework to v6.35.0 and use MoveItemsAtOnce to exchange families

--- rules/src/rules/helper/ExchangeFamilyHelper.ts
+++ rules/src/rules/helper/ExchangeFamilyHelper.ts
@@ -1,4 +1,4 @@
-import { isMoveItemType, Location, MaterialGame, MaterialMove, MaterialRulesPart } from '@gamepark/rules-api'
+import { isMoveItemTypeAtOnce, Location, MaterialGame, MaterialMove, MaterialRulesPart } from '@gamepark/rules-api'
 import { LocationType } from '../../material/LocationType'
 import { MaterialType } from '../../material/MaterialType'
 import { getFamilies } from '../../material/Tile'
@@ -13,50 +13,33 @@ export class ExchangeFamilyHelper extends MaterialRulesPart {
 
   addMovesForExchangeFamily(locationId: number): MaterialMove[] {
     const playerTiles = this.playerTilesInGame.location((loc) => loc.id === locationId)
-    const moves: MaterialMove[] = []
     if (playerTiles.length === 0) {
-      return moves
+      return []
     }
-    this.possibleEchanges(playerTiles.length).forEach((loc) => {
-      moves.push(playerTiles.minBy((item) => item.location.x!).moveItem(loc))
-    })
-    return moves
+    return this.possibleExchanges(playerTiles.length).flatMap((location) => playerTiles.moveItemsAtOnce(location))
   }
 
   switchTilesForExchange(move: MaterialMove): MaterialMove[] {
-    if (isMoveItemType(MaterialType.Tile)(move) && this.checkIfMoveIsAExchangeMove(move)) {
-      const tilesInThisLocation = this.material(MaterialType.Tile).location(
-        (loc) => loc.type === LocationType.PlayerTilesInGame && loc.id === move.location.id && loc.player === move.location.player
-      )
-      const movedTileId = this.playerTilesInGame.index(move.itemIndex).getItem()?.location.id
-      const otherPlayerTilesToMove = this.playerTilesInGame.filter((it) => it.location.id === movedTileId)
-      return [
-        tilesInThisLocation.moveItemsAtOnce({ id: movedTileId, player: this.player }),
-        otherPlayerTilesToMove.index((index) => index !== move.itemIndex).moveItemsAtOnce({ id: move.location.id, player: move.location.player })
-      ]
+    if (isMoveItemTypeAtOnce(MaterialType.Tile)(move) && move.location.player !== this.player) {
+      const tilesInThisLocation = this.material(MaterialType.Tile)
+        .location(LocationType.PlayerTilesInGame)
+        .locationId(move.location.id)
+        .player(move.location.player)
+      const originId = this.playerTilesInGame.index(move.indexes[0]).getItem()?.location.id
+      return [tilesInThisLocation.moveItemsAtOnce({ type: LocationType.PlayerTilesInGame, player: this.player, id: originId })]
     }
     return []
   }
 
-  private checkIfMoveIsAExchangeMove(move: MaterialMove): boolean {
-    return (
-      isMoveItemType(MaterialType.Tile)(move) &&
-      move.location.type === LocationType.PlayerTilesInGame &&
-      move.location.player !== this.player &&
-      move.location.x !== undefined &&
-      this.material(MaterialType.Tile).index(move.itemIndex).getItem()?.location.type === LocationType.PlayerTilesInGame
-    )
-  }
-
-  private possibleEchanges(familyLength: number): Location[] {
-    const locations: Location[] = []
+  private possibleExchanges(familyLength: number): Partial<Location>[] {
+    const locations: Partial<Location>[] = []
     this.game.players.forEach((player) => {
       if (player !== this.player) {
         const playerTiles = this.material(MaterialType.Tile).location(LocationType.PlayerTilesInGame).player(player)
         const families = getFamilies(playerTiles)
         families.forEach((family) => {
           if (family.length === familyLength) {
-            locations.push({ ...family[0].location, x: 0 })
+            locations.push({ type: LocationType.PlayerTilesInGame, player, id: family[0].location.id })
           }
         })
       }
