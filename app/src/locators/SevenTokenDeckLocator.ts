81381b4f44ff5eddb85d87c8ed2e259ce5113306
sylvestredavid <david.sylvestre.24@gmail.com>
2025-05-26 12:01:56 +0200

    afficher deck sans rotation

--- app/src/locators/SevenTokenDeckLocator.ts
+++ app/src/locators/SevenTokenDeckLocator.ts
@@ -2,8 +2,7 @@
 import { DeckLocator } from '@gamepark/react-game'
 
 class SevenTokenDeckLocator extends DeckLocator {
-  coordinates = { x: 0, y: -27 }
-  rotateZ = 90
+  coordinates = { x: 0, y: -26 }
 }
 
 export const sevenTokenDeckLocator = new SevenTokenDeckLocator()
