d2bab9847b250c01f5d65c167483561ebe42d980
Romain Fromi <romain.fromi@gmail.com>
2025-05-26 14:48:29 +0200

    Upgrade framework to v6.35.2 and add a minimum distance between tiles

--- app/src/locators/TilesPileLocator.ts
+++ app/src/locators/TilesPileLocator.ts
@@ -6,6 +6,7 @@ class TilesPileLocator extends PileLocator {
   radius = 20
   coordinates = { x: 3, y: 0 }
   limit = 91
+  minimumDistance = 2
 
   getItemCoordinates(item: MaterialItem, context: ItemContext) {
     const coordinates = super.getItemCoordinates(item, context)
